0a558c0c534a726246d50667b16722331a1d57fe
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2022-02-01 18:17:03 +0200

    adding basemap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
-import pandas as pd
-import streamlit
-import os
-import requests
-#import seaborn
+import streamlit as st
+import geemap.foliumap as geemap
+
+
+def app():
+    st.title("Change basemaps")
+
+    keys = list(geemap.ee_basemaps.keys())[1:]
+
+    basemap = st.selectbox("Select a basemap", keys)
+
+    code = f"""import geemap
+m = geemap.Map()
+m.add_basemap('{basemap}')
+m"""
+    st.code(code)
+
+    m = geemap.Map()
+    m.add_basemap(basemap)
+    m.to_streamlit()
